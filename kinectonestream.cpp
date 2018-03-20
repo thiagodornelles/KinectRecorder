95292747eaee6f3648756fce90028e98faa5f35e
thiagodornelles <thiago.azevedo87@gmail.com>
2018-03-20 20:02:03 -0300

    up

--- kinectonestream.cpp
+++ kinectonestream.cpp
@@ -43,8 +43,24 @@ void sigint_handler(int s)
     protonect_shutdown = true;
 }
 
+void removeDepthWithoutColor(cv::Mat &depth, const cv::Mat &rgb){
+    uchar r, g, b;
+    for (int i = 0; i < rgb.rows; ++i){
+        int32_t* pixel = rgb.ptr<int32_t>(i);  // point to first color in row
+        int16_t* pixelDepth = depth.ptr<int16_t>(i);
+        for (int j = 0; j < rgb.cols; ++j){
+            if(*pixel == 0){
+                *pixelDepth = 0;
+            }
+            pixel++;
+            pixelDepth++;
+        }
+
+    }
+}
+
 void DepthImage_convert_32FC1_to_16UC1(cv::Mat &dest, const cv::Mat &src, float scale) {
-//    assert(src.type() != CV_32FC1 && "DepthImage_convert_32FC1_to_16UC1: source image of different type from 32FC1");
+    //    assert(src.type() != CV_32FC1 && "DepthImage_convert_32FC1_to_16UC1: source image of different type from 32FC1");
     float *sptr = (float*)src.data;
     int size = src.rows * src.cols;
     float *send = sptr + size;
@@ -60,7 +76,7 @@ void DepthImage_convert_32FC1_to_16UC1(cv::Mat &dest, const cv::Mat &src, float 
 }
 
 void DepthImage_convert_16UC1_to_32FC1(cv::Mat &dest, const cv::Mat &src, float scale) {
-//    assert(src.type() != CV_16UC1 && "DepthImage_convert_16UC1_to_32FC1: source image of different type from 16UC1");
+    //    assert(src.type() != CV_16UC1 && "DepthImage_convert_16UC1_to_32FC1: source image of different type from 16UC1");
     const unsigned short *sptr = (const unsigned short*)src.data;
     int size = src.rows * src.cols;
     const unsigned short *send = sptr + size;
@@ -77,6 +93,8 @@ void DepthImage_convert_16UC1_to_32FC1(cv::Mat &dest, const cv::Mat &src, float 
 
 int main()
 {
+
+    long maxDistance = 2000;
     system("rm -rf data/depth");
     system("rm -rf data/rgb");
     system("mkdir data/depth");
@@ -86,7 +104,7 @@ int main()
     //! [context]
     libfreenect2::Freenect2 freenect2;
     libfreenect2::Freenect2Device *dev = 0;
-    libfreenect2::PacketPipeline *pipeline = 0;
+    libfreenect2::PacketPipeline *pipeline = new libfreenect2::OpenGLPacketPipeline();
     //! [context]
 
     //! [discovery]
@@ -141,8 +159,7 @@ int main()
     libfreenect2::Frame undistorted(512, 424, 4), registered(512, 424, 4), depth2rgb(1920, 1080 + 2, 4); // check here (https://github.com/OpenKinect/libfreenect2/issues/337) and here (https://github.com/OpenKinect/libfreenect2/issues/464) why depth2rgb image should be bigger
     //! [registration setup]
 
-    Mat rgbmat, depthmat, depthmatUndistorted, irmat, rgbd, rgbd2;
-    Mat depth16;
+    Mat rgbmat, depthmat, depthmatUndistorted, irmat, rgbd;
 
     std::cerr << "Color calibration\n";
     std::cerr << "cx: " << dev->getColorCameraParams().cx << std::endl;
@@ -163,7 +180,6 @@ int main()
         libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
         libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
         //! [loop start]
-
         //        cv::Mat(rgb->height, rgb->width, CV_8UC4, rgb->data).copyTo(rgbmat);
         //        cv::Mat(ir->height, ir->width, CV_32FC1, ir->data).copyTo(irmat);
         //        cv::Mat(depth->height, depth->width, CV_32FC1, depth->data).copyTo(depthmat);
@@ -173,15 +189,26 @@ int main()
 
         //! [registration]
         registration->apply(rgb, depth, &undistorted, &registered, true);
+
         //! [registration]
         cv::Mat(undistorted.height, undistorted.width, CV_32FC1, undistorted.data).copyTo(depthmatUndistorted);
         cv::Mat(registered.height, registered.width, CV_8UC4, registered.data).copyTo(rgbd);
+//        resize(depthmatUndistorted, depthmatUndistorted, Size(depthmatUndistorted.cols/2, depthmatUndistorted.rows/2));
+//        resize(rgbd, rgbd, Size(rgbd.cols/2, rgbd.rows/2));
+        //        cv::Mat(rgb->height, rgb->width, CV_8UC4, rgb->data).copyTo(rgbmat);
         //        cv::Mat(depth2rgb.height, depth2rgb.width, CV_32FC1, depth2rgb.data).copyTo(rgbd2);
         Mat depth16;
-        DepthImage_convert_32FC1_to_16UC1(depth16, depthmatUndistorted, 1.f);
+        threshold(depthmatUndistorted, depthmatUndistorted, maxDistance, 6000, 4);
+        DepthImage_convert_32FC1_to_16UC1(depth16, depthmatUndistorted, 1.0f);        
+        removeDepthWithoutColor(depth16, rgbd);
+
+        flip(depth16, depth16, 1);
+        flip(rgbd, rgbd, 1);
+
 
         cv::imshow("undistorted", depth16);
         cv::imshow("registered", rgbd);
+        //        cv::imshow("color", rgbmat);
 
 
         if (recording) {
@@ -190,10 +217,10 @@ int main()
             ss << setfill('0') << setw(7) << frameCount;
             ss << ".png";
             //            std::cerr << "Gravando " << ss.str() << "\n";
-//            std::vector<int> compression_params;
-//            compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
-//            compression_params.push_back(0);
-//            cv::imwrite(ss.str(), depthmatUndistorted, compression_params);
+            //            std::vector<int> compression_params;
+            //            compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+            //            compression_params.push_back(0);
+            //            cv::imwrite(ss.str(), depthmatUndistorted, compression_params);
             cv::imwrite(ss.str(), depth16);
 
             //limpar o ss
@@ -214,6 +241,15 @@ int main()
 
         if(key == 'r' || key == 'R'){
             recording = !recording;
+        }
+
+        if(key == '-'){
+            maxDistance -= 10;
+            cerr << maxDistance << endl;
+        }
+        if(key == '='){
+            maxDistance += 10;
+            cerr << maxDistance << endl;
         }
 
         //! [loop end]
